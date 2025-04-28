9a87189ebb0c29f1d34d4c1965709a082a8e2523
sam06tanb <samirtajra53@gmail.com>
2025-04-28 01:06:32 -0300

    feat: Dto validation added

--- src/main/java/dev/samir/backangulart/api/dto/ClothDto.java
+++ src/main/java/dev/samir/backangulart/api/dto/ClothDto.java
@@ -1,9 +1,18 @@
 package dev.samir.backangulart.api.dto;
 
 import dev.samir.backangulart.domain.EnumCloth;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ClothDto(
+
+        @NotBlank(message = "Name cannot be empty")
         String name,
+
+        @NotBlank(message = "Color cannot be empty")
         EnumCloth size,
+
+        @NotNull(message = "Size must be provided")
         String color
+
 ) {}
